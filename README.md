f6b2204a843843a1c6423638ee3d19c2039b5c52
kinteriq <kinteriq@gmail.com>
2019-10-17 00:18:50 +0500

    Initialize

---
+++ README.md
@@ -0,0 +1,3 @@
+This package parses SMTP-server's log and collects following data:
+    - the amount of sent mail linked to each client's mail address;
+    - the amount of delivered and undelivered mail.
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mail-log-parser",
+    version="0.0.1",
+    author="kinteriq",
+    author_email="kinteriq@gmail.com",
+    description="Package for parsing mail logs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kinteriq/mail-log-parser",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
